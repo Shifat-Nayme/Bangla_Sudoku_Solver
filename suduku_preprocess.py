d559ed2d0356873c6198e24b9739e2610b8d784b
Shifat Nayme <39239744+Shifat-Nayme@users.noreply.github.com>
2020-06-15 00:40:57 +0600

    Add files via upload
    The preprocessing step has been done.

---
+++ suduku_preprocess.py
@@ -0,0 +1,160 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import operator
+
+
+def display_points(in_img, points, radius=5, colour=(0, 0, 255)):
+	"""Draws circular points on an image."""
+	img = in_img.copy()
+
+	# Dynamically change to a colour image if necessary
+	if len(colour) == 3:
+		if len(img.shape) == 2:
+			img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+		elif img.shape[2] == 1:
+			img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+	for point in points:
+		img = cv2.circle(img, tuple(int(x) for x in point), radius, colour, -1)
+	show_image(img)
+	return img
+
+
+def find_corners_of_largest_polygon(img):
+	"""Finds the 4 extreme corners of the largest contour in the image."""
+	contours, h = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # Find contours
+	contours = sorted(contours, key=cv2.contourArea, reverse=True)  # Sort by area, descending
+	polygon = contours[0]  # Largest image
+
+	# Use of `operator.itemgetter` with `max` and `min` allows us to get the index of the point
+	# Each point is an array of 1 coordinate, hence the [0] getter, then [0] or [1] used to get x and y respectively.
+
+	# Bottom-right point has the largest (x + y) value
+	# Top-left has point smallest (x + y) value
+	# Bottom-left point has smallest (x - y) value
+	# Top-right point has largest (x - y) value
+	bottom_right, _ = max(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+	top_left, _ = min(enumerate([pt[0][0] + pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+	bottom_left, _ = min(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+	top_right, _ = max(enumerate([pt[0][0] - pt[0][1] for pt in polygon]), key=operator.itemgetter(1))
+
+	# Return an array of all 4 points using the indices
+	# Each point is in its own array of one coordinate
+	return [polygon[top_left][0], polygon[top_right][0], polygon[bottom_right][0], polygon[bottom_left][0]]
+
+
+
+def plot_many_images(images, titles, rows=1, columns=2):
+	for i, image in enumerate(images):
+		plt.subplot(rows, columns, i+1)
+		plt.imshow(image, 'gray')
+		plt.title(titles[i])
+		plt.xticks([]), plt.yticks([])  # Hide tick marks
+	plt.show()
+
+def show_image(img):
+	cv2.imshow("image",img)
+	cv2.waitKey(0)  
+	cv2.destroyAllWindows()
+
+
+def pre_process_image(img, skip_dilate=False):
+    proc = cv2.GaussianBlur(img.copy(), (9, 9), 0)
+    proc = cv2.adaptiveThreshold(proc, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    proc = cv2.bitwise_not(proc, proc)
+    if not skip_dilate:
+        kernel = np.array([[0., 1., 0.], [1., 1., 1.], [0., 1., 0.]],np.uint8)
+        proc = cv2.dilate(proc, kernel)
+    return proc
+
+
+def distance_between(p1, p2):
+	"""Returns the scalar distance between two points"""
+	a = p2[0] - p1[0]
+	b = p2[1] - p1[1]
+	return np.sqrt((a ** 2) + (b ** 2))
+
+def crop_and_warp(img, crop_rect):
+	"""Crops and warps a rectangular section from an image into a square of similar size."""
+
+	# Rectangle described by top left, top right, bottom right and bottom left points
+	top_left, top_right, bottom_right, bottom_left = crop_rect[0], crop_rect[1], crop_rect[2], crop_rect[3]
+
+	# Explicitly set the data type to float32 or `getPerspectiveTransform` will throw an error
+	src = np.array([top_left, top_right, bottom_right, bottom_left], dtype='float32')
+
+	# Get the longest side in the rectangle
+	side = max([
+		distance_between(bottom_right, top_right),
+		distance_between(top_left, bottom_left),
+		distance_between(bottom_right, bottom_left),
+		distance_between(top_left, top_right)
+	])
+
+	# Describe a square with side of the calculated length, this is the new perspective we want to warp to
+	dst = np.array([[0, 0], [side - 1, 0], [side - 1, side - 1], [0, side - 1]], dtype='float32')
+
+	# Gets the transformation matrix for skewing the image to fit a square by comparing the 4 before and after points
+	m = cv2.getPerspectiveTransform(src, dst)
+
+	# Performs the transformation on the original image
+	return cv2.warpPerspective(img, m, (int(side), int(side)))
+
+
+def infer_grid(img):
+	"""Infers 81 cell grid from a square image."""
+	squares = []
+	side = img.shape[:1]
+	side = side[0] / 9
+	for i in range(9):
+		for j in range(9):
+			p1 = (i * side, j * side)  # Top left corner of a bounding box
+			p2 = ((i + 1) * side, (j + 1) * side)  # Bottom right corner of bounding box
+			squares.append((p1, p2))
+	return squares
+
+def display_rects(in_img, rects, colour=255):
+	"""Displays rectangles on the image."""
+	img = in_img.copy()
+	for rect in rects:
+		img = cv2.rectangle(img, tuple(int(x) for x in rect[0]), tuple(int(x) for x in rect[1]), colour)
+	show_image(img)
+	return img
+
+
+img = cv2.imread('suduku.png', cv2.IMREAD_GRAYSCALE)
+show_image(img)
+processed = pre_process_image(img)
+#show_image(processed)
+
+#ext_contours, hier = cv2.findContours(processed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#contours, hier = cv2.findContours(processed.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+#processed = cv2.cvtColor(processed, cv2.COLOR_GRAY2RGB)
+#all_contours = cv2.drawContours(processed.copy(), contours, -1, (255, 0, 0), 2)
+#external_only = cv2.drawContours(processed.copy(), ext_contours, -1, (255, 0, 0), 2)
+#plot_many_images([all_contours, external_only], ['All Contours', 'External Only'])
+
+
+corners = find_corners_of_largest_polygon(processed)
+display_points(processed, corners)
+cropped = crop_and_warp(img, corners)
+cv2.imshow("cropped",cropped)
+cv2.waitKey(0)
+cv2.imwrite('cropped.jpg', cropped)
+
+squares = infer_grid(cropped)
+pre=display_rects(cropped, squares)
+cv2.imshow("display_rect",pre)
+cv2.waitKey(0)
+
+
+
+
+
+
+
+
+
+
+
